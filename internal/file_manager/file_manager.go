22378f115af2b932de1a79ffbe938d1b17c5d346
himanshu-sah <itsmehs07@gmail.com>
2022-11-01 18:15:37 +0530

    file manager, abstraction over file system for get put delete and ifexists

---
+++ internal/file_manager/file_manager.go
@@ -0,0 +1,46 @@
+package file_manager
+
+import (
+	"io/fs"
+	"os"
+	"path"
+)
+
+type FileManager struct {
+	rootDir string
+}
+
+func New(rootDir string) *FileManager {
+	return &FileManager{
+		rootDir: rootDir,
+	}
+}
+
+func (fm *FileManager) Get(filePath []string) ([]byte, error) {
+	finalFilePath := path.Join(fm.rootDir, path.Join(filePath...))
+	return os.ReadFile(finalFilePath)
+}
+
+func (fm *FileManager) Put(filePath []string, data []byte) error {
+	finalFilePath := path.Join(fm.rootDir, path.Join(filePath...))
+	file, err := os.Create(finalFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (fm *FileManager) IfFileExists(filePath []string) (fs.FileInfo, error) {
+	finalFilePath := path.Join(fm.rootDir, path.Join(filePath...))
+	return os.Stat(finalFilePath)
+}
+
+func (fm *FileManager) Delete(filePath []string) error {
+	finalFilePath := path.Join(fm.rootDir, path.Join(filePath...))
+	return os.Remove(finalFilePath)
+}
---
+++ internal/file_manager/file_manager_test.go
@@ -0,0 +1,29 @@
+package file_manager
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFileManager(t *testing.T) {
+
+	fm := New(".")
+	fileName := []string{"new.txt"}
+	data := []byte("hello")
+	err := fm.Put(fileName, data)
+
+	require.NoError(t, err)
+
+	readData, err := fm.Get(fileName)
+	require.NoError(t, err)
+	require.NotEmpty(t, readData)
+	require.Equal(t, data, readData)
+
+	err = fm.Delete(fileName)
+	require.NoError(t, err)
+
+	_, err = fm.IfFileExists(fileName)
+	require.Error(t, err)
+
+}

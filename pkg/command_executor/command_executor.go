cf9a890dfde5af2570a26d83dd4cc4e28393e5d0
sunkoshi <itsmehs07@gmail.com>
2022-11-27 18:12:29 +0530

    remove taking shell as param and take working directory as the param

--- pkg/command_executor/command_executor.go
+++ pkg/command_executor/command_executor.go
@@ -6,13 +6,10 @@ import (
 	"os/exec"
 )
 
-const SH_SHELL = "sh"
-const BASH_SHELL = "bash"
-const ZSH_SHELL = "zsh"
-
-func ExecuteContext(ctx context.Context, shell string, rawCmd string) (string, string, error) {
+func ExecuteContext(ctx context.Context, dir string, rawCmd string) (string, string, error) {
 	cmd := exec.CommandContext(ctx, "bash", "-c", rawCmd)
 	var outputBuffer, errorBuffer bytes.Buffer
+	cmd.Dir = dir
 	cmd.Stdout = &outputBuffer
 	cmd.Stderr = &errorBuffer
 	err := cmd.Run()
